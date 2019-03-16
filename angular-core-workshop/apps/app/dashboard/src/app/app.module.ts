49e716e2e0c7d1d3f3cdbdf15b87bed4b5d26416
almg_walsh <aidan.walsh@haymarket.com>
2019-03-16 17:01:28 +0000

    first commit

---
+++ angular-core-workshop/apps/app/dashboard/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@workshop/material';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomersComponent } from './customers/customers.component';
+import { HomeModule } from './home/home.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CustomersModule } from './customers/customers.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    HomeModule,
+    ProjectsModule,
+    CustomersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
